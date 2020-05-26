b2a6dc8c39e88ef05cff0e53a2df21da88b83c4b
hudaman0621 <mrhu828@gmail.com>
2020-05-26 15:32:59 -0400

    post artist works. saving progress

--- app.js
+++ app.js
@@ -13,15 +13,89 @@ const db = require('./models');
 
 
 //routes
-app.get('/', (req, res) => {
+app.get('/index', (req, res) => {
+    console.log('Hello');
+    // res.json(result);
+    // db.Artist.findAll().then((result) => {
+    //     res.json(result);
+    // })
+})
+
+app.get('/artist', (req, res) => {
     db.Artist.findAll().then((result) => {
         res.json(result);
     })
 })
-app.get('/:id', (req, res) => {
+app.get('/artist/:id', (req, res) => {
     db.Artist.findByPk(req.params.id).then(result => {
         res.json(result);
     });
 });
+
+app.get('/album', (req, res) => {
+    db.Album.findAll().then((result) => {
+        res.json(result);
+    });
+});
+
+app.get('/album/:id', (req, res) => {
+    db.Album.findByPk(req.params.id).then((result) => {
+        res.json(result);
+    });
+});
+
+app.get('/track', (req, res) => {
+    db.Track.findAll().then((result) => {
+        res.json(result);
+    });
+});
+
+app.get('/track/:id', (req, res) => {
+    db.Track.findByPk(req.params.id).then((result) => {
+        res.json(result);
+    });
+});
+
+// app.post('/newartist', (req, res) => {
+//     const name = req.body.name;
+
+//     db.Artist.create({
+//         name: name
+//     }).then((result) => {
+//         res.json(result);
+//     });
+// });
+
+//create artist
+app.post('/artist', (req, res) => {
+    db.Artist.create({
+        name: req.body.name,
+    }).then(result => {
+        res.json(result);
+    });
+});
+
+app.post('/artist/:artist_id/album', (req, res) => {
+    db.Artist.findByPk(req.params.artist_id)
+    .then(artist => {
+        return artist.createAlbum({
+            name: req.body.name,
+            year: req.body.year,
+        })
+    .then((result) => {
+        res.json(result);
+    })
+    })
+});
+
+app.post('/artist/:artist_id/album/:album_id/track', (req, res) => {
+    db.Album.findByPk(req.params.album_id)
+        .then(album => {
+            return album.createTrack({
+                name: req.body.name,
+                duration: req.body.duration,
+            })
+        })
+})
 
 app.listen(PORT, () => console.log(`The Server is running on Http://localhost:${PORT}`));
--- migrations/20200526190536-create-album.js
+++ migrations/20200526190536-create-album.js
@@ -1,14 +1,14 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Albumns', {
+    return queryInterface.createTable('Albums', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      albumn_name: {
+      album_name: {
         type: Sequelize.STRING
       },
       createdAt: {
@@ -22,6 +22,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Albumns');
+    return queryInterface.dropTable('Albums');
   }
 };
